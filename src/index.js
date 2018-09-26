bb4e94899884c97c44ccea935601ff2b16808791
Student <example@example.com>
2018-09-26 09:33:17 -0700

    Add google map to front page (broken work in progress).

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
-
+// Carousel init
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instance = M.Carousel.init(elems, {
@@ -20,3 +20,25 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(autoplay, 4500);
   }
 });
+
+// Google map init
+// function initMap() {
+//   var nickysLoc = {
+//     lat: 40.762221,
+//     lng: -73.980320
+//   }
+//
+//   var map = new google.maps.Map(
+//     document.getElementById('map'),
+//     {
+//       zoom: 4,
+//       center: nickysLoc
+//     }
+//   );
+//
+//   var marker = new google.maps.Marker({
+//     position: nickysLoc,
+//     map: map
+//   });
+//
+// }
